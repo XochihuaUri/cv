026d50f8d3dc49aa813542891f592bebde65b606
Uriel Xochihua <urielxochi51@gmail.com>
2022-02-17 17:09:52 -0600

    cv first ed

---
+++ script.js
@@ -0,0 +1,6 @@
+//aboutme
+//background-image: url("https://cdn.pixabay.com/photo/2018/09/16/22/08/software-3682509_960_720.jpg");
+//background-size: cover;
+//skills
+//background-image: url("https://cdn.pixabay.com/photo/2018/05/17/08/34/matrix-3408060_960_720.jpg");
+//background-size: cover;
